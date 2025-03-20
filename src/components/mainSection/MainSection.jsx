aae93277d1791be0c4a9c978e042715e2adcec5f
Mukul Singh <singhmukulanand098@gmail.com>
2025-03-20 14:11:55 +0530

    Added base url in vite config

--- src/components/mainSection/MainSection.jsx
+++ src/components/mainSection/MainSection.jsx
@@ -3,7 +3,7 @@ import HorizontalSection from "../horizontalSection/HorizontalSection";
 
 const MainSection = ({ newRelease, categories }) => {
   return (
-    <main className="bg-gray-900 p-6 min-h-lvh border-2 border-amber-50">
+    <main className="bg-gray-900 p-6 min-h-lvh">
       <h2 className="text-2xl text-white">Latest Release</h2>
       <HorizontalSection itemArray={newRelease} />
       <h2 className="text-2xl text-white">Categories</h2>
